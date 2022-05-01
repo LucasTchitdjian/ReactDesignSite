a4ae2b7d8f2bc7d1564521aff969d882b351936e
Lucas TCHITDJIAN <tchitdjian.lucas@gmail.com>
2022-05-01 18:03:23 +0200

    end of project error

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,17 @@
 module.exports = {
   siteMetadata: {
     title: 'Design + Code 3',
+    description: 'A GatsbyJS starter for Design + Code 3',
+    keywords: 'gatsbyjs, gatsby, javascript, react, design, code, web, ux, ui, frontend',
   },
-  plugins: ['gatsby-plugin-react-helmet'],
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    {
+      resolve: 'gatsby-source-contentful',
+      options: {
+        spaceId: 'lo3327zfooo0',
+        accessToken: 'mQli6oiUSiL_Tvl8XUY1Psijln78Vp0Hx3DGs1gtUUg'
+      }
+    }
+  ],
 }
